aa06e992f5bcbc54f58e30d078f353557683b6d4
Alberto A. Colón Reyes <alberto_colon@me.com>
2023-09-30 12:54:58 -0400

    Add the code for the A2-f Read interpretation ex.

--- Sept-26_Files_2_Abscent/File_Example_A2-f_Read-Interpretation/readInterpretation.cpp
+++ Sept-26_Files_2_Abscent/File_Example_A2-f_Read-Interpretation/readInterpretation.cpp
@@ -1,1 +1,32 @@
 // A2-f Professor's Annotations - Files (Read and Interpretation of what was read) -- Archivos (Lectura e interpretación de lo leído)
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream file;
+    int character = 0, digit = 0;
+    char ch;
+
+    file.open("numeros.txt");
+    if(file.fail())
+        cout << "Error: This file could not be open: " << "numeros.txt" << endl;
+    else
+    {
+        ch = file.get();
+        while(!file.eof())
+        {
+            ++character;
+            if(ch >= '0' && ch <= '9')
+                ++digit;
+            ch = file.get();
+        }
+
+        cout << "There is " << character << " characters on the file." << endl << "There's also, " << digit << " digits on the file." << endl;
+        file.close();
+    }
+
+    return 0;
+}
