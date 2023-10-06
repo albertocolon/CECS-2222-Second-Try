00b5a7c310211255f89316c0cee3225498ee473f
Alberto A. Colón Reyes <alberto_colon@me.com>
2023-10-05 20:11:59 -0400

    Update BatteryBag.cpp
    Add part of the definitions code

--- Examen-2/Baterry-Class/Battery-1/BatteryBag.cpp
+++ Examen-2/Baterry-Class/Battery-1/BatteryBag.cpp
@@ -2,5 +2,61 @@
 // Prof. Luis Ortiz - FA - 23
 // Alberto A. Colón Reyes - ID# - 67689
 // Oct. 1 2023
-// Problem 2 - Battery Bag Class - Definitions
+// Problem 2 - Battery Bag Class - Declaration
 
+#include <iostream>
+#include "BatteryBag.h"
+
+using namespace std;
+
+const Battery & BatteryBag::operator [](int index) const
+{
+    this->
+}
+
+Battery & BatteryBag::operator [](int index)
+{
+
+}
+
+void BatteryBag::addBattery(const Battery & aBattery)
+{
+
+}
+
+BatteryBag::BatteryBag()
+{
+
+}
+
+BatteryBag::BatteryBag(const Battery & aBattery)
+{
+
+}
+
+BatteryBag::BatteryBag(const BatteryBag & aBag)
+{
+
+}
+
+BatteryBag::~BatteryBag()
+{
+
+}
+
+BatteryBag & BatteryBag::operator =(const BatteryBag & aBag);
+BatteryBag & BatteryBag::operator =(const Battery & aBattery);
+
+BatteryBag & BatteryBag::operator +(const BatteryBag & aBag);
+BatteryBag & BatteryBag::operator +(const Battery & aBattery);
+
+BatteryBag & BatteryBag::operator -(const BatteryBag & aBag);
+BatteryBag & BatteryBag::operator -(const Battery & aBattery);
+
+bool BatteryBag::operator ==(const BatteryBag & aBag) const;
+bool BatteryBag::operator !=(const BatteryBag & aBag) const;
+
+double BatteryBag::totalCharge() const;
+
+ostream & operator <<(ostream & out, const BatteryBag & aBag);
+istream & operator >>(istream & in, BatteryBag & aBag);
